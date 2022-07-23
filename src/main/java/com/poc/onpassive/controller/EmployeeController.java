482e9dbbcd69fda64e50780c9f4256468f3e5afc
venu <gvenu350@gmail.com>
2022-07-23 18:49:14 +0530

    third commit

--- src/main/java/com/poc/onpassive/controller/EmployeeController.java
+++ src/main/java/com/poc/onpassive/controller/EmployeeController.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,27 +11,26 @@ import org.springframework.web.bind.annotation.RestController;
 import com.poc.onpassive.entity.Employee;
 import com.poc.onpassive.services.EmployeeService;
 
-
 @RestController
-public class EmployeeController  {
+public class EmployeeController {
+
 	@Autowired
 	private EmployeeService employeeService;
 
 	@PostMapping("/employee")
 	public Employee createEmployee(@RequestBody Employee employee) {
-		 Employee save = employeeService.save(employee);
-		 System.err.println("save::::"+save);
-		 return save;
+		Employee save = employeeService.save(employee);
+		System.err.println("save::::" + save);
+		return save;
 	}
-	
+
 	@GetMapping("/viewemployeelist")
-	public List<Employee> ViewEmployee()
-	{
+	public List<Employee> ViewEmployee() {
 		System.out.println("hellow world");
-		List<Employee> view =employeeService.viewEmployee();
+		List<Employee> view = employeeService.viewEmployee();
 		return view;
 	}
-	
+
 //	@PostMapping("/viewbyid")
 //	public  void view(@RequestBody Employee employee,@PathVariable("id") Long id)
 //	{
